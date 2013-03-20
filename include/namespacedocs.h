3c2a8caf3232e7dec3ccda63dcea2938cf175b6f
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-03-20 16:11:30 +0000

    Added the skeleton of the new CSEditing library and moved all editor relevant stuff from CS into CSEditing.

---
+++ include/namespacedocs.h
@@ -0,0 +1,50 @@
+/*
+  Copyright (C) 2007 by Marten Svanfeldt
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef CS_NAMESPACEDOCS_H
+#define CS_NAMESPACEDOCS_H
+
+/** \file
+ * Documentation for namespaces in CSEditing
+ */
+
+/**
+ * Main namespace for the Crystal Space editing environment
+ */
+namespace CSEE
+{
+  /**
+   * Graphical User Interface tools for the edition of Crystal Space objects.
+   */
+  namespace Editor
+  {
+    /**
+     * Default components for the cseditor framework.
+     */
+    namespace Component
+    {}
+
+    /**
+     * Core components of the cseditor framework.
+     */
+    namespace Core
+    {}
+  }
+}
+
+#endif
