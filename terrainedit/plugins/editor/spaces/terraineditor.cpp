0f8bf1b578da33a7bbde30a9416ac88e0df097b9
soumitra1993 <soumitra1993@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2014-08-17 18:22:47 +0000

    Tested Parsing of cell using world.xml standards | Added proper comments to both the header and the cpp file

--- terrainedit/plugins/editor/spaces/terraineditor.cpp
+++ terrainedit/plugins/editor/spaces/terraineditor.cpp
@@ -71,9 +71,6 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 {
   CSTerrainEditSpace* terrainEditorSpace;
 
-  /// Helper function; apparently this functionality isn't built into wx
-
-
   // This table triggers a bunch of MFP conversion warnings, but due to
   // the fact that the wxPanel is the first class being inherited, there
   // shouldn't be any problems.
@@ -120,27 +117,18 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
     // Initializes the wxPanel part of the space
     Create (parent);
 
-    // Load the particle plugins
+    // Load the Terrain plugins
     csRef<iPluginManager> pluginManager = csQueryRegistry<iPluginManager> (object_reg);
 
+    //Load the Decal Plugin.
     csInitializer::RequestPlugins (object_reg, CS_REQUEST_PLUGIN ("crystalspace.decal.manager",iDecalManager),CS_REQUEST_END);
-    /*
-    feeder = csLoadPluginCheck<iTerrainDataFeeder>
-    (pluginManager, "crystalspace.mesh.object.terrain2.modifiabledatafeeder");
-
-    if(!feeder)
-    {
-      printf("no feeder found");
-    }
-    */    
+        
     // Setup the event names
-    nameRegistry = csEventNameRegistry::GetRegistry (object_reg);
-    //addObject = nameRegistry->GetID ("crystalspace.editor.context.selection.addselectedobject");
-    //clearObjects = nameRegistry->GetID ("crystalspace.editor.context.selection.clearselectedobjects");
+    nameRegistry = csEventNameRegistry::GetRegistry (object_reg);    
     activateObject = nameRegistry->GetID ("crystalspace.editor.context.selection.setactiveobject");
        
     // Respond to context events
-    //csEventID contextSelect = nameRegistry->GetID ("crystalspace.editor.context");
+    
     RegisterQueue (editor->GetContext ()->GetEventQueue (), activateObject);
 
     // Register a mouse listener to the global event queue
@@ -170,37 +158,37 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 
     wxButton* but = new wxButton (this, idButtonAddCell, wxT ("Add Cell"));
     but->SetSize (-1, 32);
-    middleLSizer->Add ( but,
+    middleRSizer->Add ( but,
                         0,
                         wxALL | wxEXPAND,
                         borderWidth );
 
     wxButton* but2 = new wxButton (this, idButtonDeleteCell, wxT ("DeleteCell"));
     but2->SetSize (-1, 32);
-    middleLSizer->Add ( but2,
+    middleRSizer->Add ( but2,
                         0,
                         wxALL | wxEXPAND,
                         borderWidth );
 
     wxRadioButton* butDefault = new wxRadioButton (this , idButtonDefaultMode , wxT ("Default"));
-    middleLSizer->Add ( butDefault,
+    middleRSizer->Add ( butDefault,
                         0,
                         wxALL | wxEXPAND,
                         borderWidth );
 
     wxRadioButton* butPaint = new wxRadioButton (this , idButtonPaintMode , wxT ("Paint"));
-    middleLSizer->Add ( butPaint,
+    middleRSizer->Add ( butPaint,
                         0,
                         wxALL | wxEXPAND,
                         borderWidth );
 
     wxRadioButton* butModifier = new wxRadioButton (this , idButtonModifierMode , wxT ("Modifier"));
-    middleLSizer->Add ( butModifier,
+    middleRSizer->Add ( butModifier,
                         0,
                         wxALL | wxEXPAND,
                         borderWidth );
 
-    /*
+    /* For future use
     wxButton* but3 = new wxButton (this, idButtonCreateMeshGenerator, wxT ("Mesh"));
     but3->SetSize (-1, 32);
     middleLSizer->Add ( but3,
@@ -208,14 +196,14 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
                         wxALL | wxEXPAND,
                         borderWidth );
     */
-    middleRSizer->Add (new wxStaticText (this, wxID_ANY, wxT ("Materials")));
+    middleLSizer->Add (new wxStaticText (this, wxID_ANY, wxT ("Materials")));
     matList = new wxListBox (this,idMatList);
-    middleRSizer->Add ( matList,
+    middleLSizer->Add ( matList,
                         1,
                         wxALL | wxEXPAND,
                         borderWidth );
 
-                
+    //Secondary Editor
     secondaryEditor = new ModifiableEditor (object_reg, this, idSecondaryEditor,
       wxDefaultPosition, wxDefaultSize, 0L, wxT ("Secondary editor"));
 
@@ -248,7 +236,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
       scfQueryInterface<iContextObjectSelection> (editor->GetContext ());
        
 
-    // Search for the iModifiable interface of the particle factory
+    // Search for the iModifiable interface of the Terrain factory
     result = objectSelectionContext->GetActiveObject ();
     if (!result)
     {
@@ -256,8 +244,6 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
       return;
     }
       
-    //csString entityName (result->GetName ());
-
     csRef<iMeshWrapper> mesh = scfQueryInterface<iMeshWrapper> (result);
     if (!mesh)
     {
@@ -281,22 +267,32 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
       return;
     }
 
-    //terrainFactory->SetFeeder(feeder);
- 	
- 	  factory = terrainFactory;
- 	  terrain = terrainSys;
+    char cellName  = 0;
+    terrainFactoryCell = scfQueryInterface<iTerrainFactoryCell> (terrainFactory->GetCell(cellName));
+    if (!terrainFactoryCell)
+    {
+      printf("No terrainFactoryCell Found");
+    }
+
+    //Setting the queried Terrain Factory as the global factory.
+    factory = terrainFactory;
+
+    factory->SetRenderer( terrainFactory->GetRenderer());
+    factory->SetCollider( terrainFactory->GetCollider());
+    factory->SetFeeder( terrainFactory->GetFeeder());
+
+    //Setting the queried Terarin System as the global system.
+    terrain = terrainSys;
     UpdateCellList ();
 
     
     csRef<iModifiable> modifiable = scfQueryInterface<iModifiable> (mesh->GetMeshObject()->GetFactory());  
+    
     if (!modifiable)
     {
-      printf("Selected particle system is non-standard and doesn't implement iModifiable. It cannot be edited.");
+      printf("Selected Terrain system is non-standard and doesn't implement iModifiable. It cannot be edited.");
       return;
     }
-
-    // Caches a casted pointer to the factory
-    
    
     // Updates the GUI
     mainEditor->SetModifiable (modifiable);
@@ -304,6 +300,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
     // Setup the decal
     decalManager = csQueryRegistry<iDecalManager> (object_reg);
 
+    //Queries the engine
     csRef<iEngine> engine = csQueryRegistry<iEngine> (object_reg);
 
     if(!engine)
@@ -311,33 +308,18 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
       printf("engine not found");
     }
 
+    //Queries the material list.
     materialList = engine->GetMaterialList ();
     UpdateMaterialList();
 
-    activeMat = materialList->Get(0);
-
-    
-    loader = csQueryRegistry<iLoader>(object_reg);
-    if(!loader)
-    {
-      printf("no loader found");
-    }
-
-    loader->LoadTexture("grass.png", "/lev/terrain/grass.png");  
-    /*
-    iMaterialWrapper* material = engine->GetMaterialList ()->FindByName ("box1");
-
-    if(!material)
-    {
-      printf("Grass not found \n");
-
-    }    
-    */
+    //Sets a default activeMaterial
+    activeMat = materialList->Get(0);      
    
-   
+    //Default modifier dimensions.
     rectSize = 50.0f;
     rectHeight = 20.0f;
 
+    //Set mouse events at ready and mode to default.
     mouseready = 1;
     mode = 0;
 
@@ -347,9 +329,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
   {
     mainEditor->Clear ();
     secondaryEditor->Clear ();
-    cellList->Clear();
-    
-    //mainEditor->SetMessage (wxString (wxT ("Notice:")), message);
+    cellList->Clear();   
   }
 
 
@@ -387,42 +367,75 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
   void CSTerrainEditSpace::OnButtonAddCell ( wxCommandEvent &event )
   {
     // TODO: context menu to pick
-      printf("Add Cell");
+      //printf("Add Cell");
 
       csRef<iEngine> engine = csQueryRegistry<iEngine> (object_reg);
-      csRef<iMaterialWrapper> terrainmat = engine->CreateMaterial ("terrain", 0);
-
-      ParseCell();
-
+      
       csRef<iTerrainFactoryCell> cellf(factory->AddCell ());
-     //defining new cell properties 
+     
+     //Defining new cell properties using world.xml standards - START 
       cellf->SetName ("2");    
       cellf->SetPosition (csVector2 (-128.0f + (factory->GetCellCount () - 1) * 256.0f, -128.0f));
       cellf->SetSize (csVector3 (256.0f, 16.0f, 256.0f));
       cellf->SetGridWidth (257);
       cellf->SetGridHeight (257);
-      cellf->SetMaterialMapWidth (256);
-      cellf->SetMaterialMapHeight (256);
-      cellf->SetMaterialPersistent (false);
+      cellf->SetMaterialMapWidth (512);
+      cellf->SetMaterialMapHeight (512);
+      cellf->SetMaterialPersistent (terrainFactoryCell->GetMaterialPersistent());
 
-      cellf->SetBaseMaterial (terrainmat);
+      cellf->SetBaseMaterial (terrainFactoryCell->GetBaseMaterial());
+      cellf->SetSplatBaseMaterial (terrainFactoryCell->GetSplatBaseMaterial());
+      cellf->SetAlphaSplatMaterial (terrainFactoryCell->GetAlphaSplatMaterial());
 
-      cellf->GetRenderProperties ()->SetParameter ("block resolution", "16");
+      csRef<iShaderVarStringSet> stringSet = csQueryRegistryTagInterface<iShaderVarStringSet>(object_reg, "crystalspace.shader.variablenameset");
+
+      if(!stringSet)
+      {
+        printf("No strings found");
+      }
+  
+      CS::ShaderVarStringID lightmap = stringSet->Request ("tex lightmap");
+      CS::ShaderVarStringID lightmap_dir_1 = stringSet->Request ("tex lightmap dir 1");
+      CS::ShaderVarStringID lightmap_dir_2 = stringSet->Request ("tex lightmap dir 2");
+      CS::ShaderVarStringID lightmap_dir_3 = stringSet->Request ("tex lightmap dir 3");
+  
+      csRef<csShaderVariable> sv;
+
+      sv.AttachNew (new csShaderVariable(lightmap));
+      cellf->GetRenderProperties ()->AddVariable (sv);
+
+      sv.AttachNew (new csShaderVariable(lightmap_dir_1));
+      cellf->GetRenderProperties ()->AddVariable (sv);
+
+      sv.AttachNew (new csShaderVariable(lightmap_dir_2));
+      cellf->GetRenderProperties ()->AddVariable (sv);
+
+      sv.AttachNew (new csShaderVariable(lightmap_dir_3));
+      cellf->GetRenderProperties ()->AddVariable (sv);
+
+
+      cellf->GetRenderProperties ()->SetVisible (1);
+      cellf->GetRenderProperties ()->SetParameter ("tex lightmap", terrainFactoryCell->GetRenderProperties()->GetParameterValue("tex lightmap"));
+      cellf->GetRenderProperties ()->SetParameter ("tex lightmap dir 1", terrainFactoryCell->GetRenderProperties()->GetParameterValue("tex lightmap dir 1"));
+      cellf->GetRenderProperties ()->SetParameter ("tex lightmap dir 2", terrainFactoryCell->GetRenderProperties()->GetParameterValue("tex lightmap dir 2"));
+      cellf->GetRenderProperties ()->SetParameter ("tex lightmap dir 3", terrainFactoryCell->GetRenderProperties()->GetParameterValue("tex lightmap dir 3"));
+      cellf->GetRenderProperties ()->SetParameter ("basemap scale", terrainFactoryCell->GetRenderProperties()->GetParameterValue("basemap scale"));
       cellf->GetRenderProperties ()->SetParameter ("lod splitcoeff", "16");
 
-      cellf->GetFeederProperties ()->SetParameter("heightmap source", "/lev/terrain/heightmap.png");
-      cellf->GetFeederProperties ()->SetParameter("materialmap source", "/lev/terrain/materialmap.png");
+      cellf->GetFeederProperties ()->SetParameter ("heightmap source", "/lev/terrain/heightmap.png");
+      cellf->GetFeederProperties ()->SetParameter ("materialmap source", "/lev/terrain/materialmap.png");
+
     //definition ends.  
   
-    csRef<iTerrainCell> cell (terrain->AddCell(cellf));
-    UpdateCellList();
+      csRef<iTerrainCell> cell (terrain->AddCell(cellf));
+      UpdateCellList();
   }
 
   void CSTerrainEditSpace::OnCellSelect ( wxCommandEvent& event )
   {
     
     iTerrainFactoryCell* cell = static_cast<iTerrainFactoryCell*> (cellList->GetClientData (event.GetSelection ()));
-    mod = scfQueryInterface<iModifiable> (cell);
+    mod = scfQueryInterface<iModifiable> (cell); //Secondary Modifiable
 
     csRef<CS::Utility::iModifiableListener> listener;
     listener.AttachNew (new ModifiableListener ());
@@ -482,7 +495,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 
   void CSTerrainEditSpace::OnButtonCreateMeshGenerator (wxCommandEvent &event ) 
   {
-
+    ///For the Future
     csRef<iContextCamera> contextCamera = scfQueryInterface<iContextCamera> (editor->GetContext ());
     iCamera* camera = contextCamera->GetCamera();
 
@@ -498,28 +511,30 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 
     iMeshGenerator* meshGen = sector->CreateMeshGenerator("newGenerator");
     meshGen->AddMesh(meshWrapper);
-    iMeshGeneratorGeometry* geometry= meshGen->CreateGeometry();    
+    //iMeshGeneratorGeometry* geometry= meshGen->CreateGeometry();    
   }
 
   void CSTerrainEditSpace::UpdateModifier (int x, int y)
   {
-    //printf("%i \t %i \n", x , y);
+    //Query the Data Feeder
     csRef<iModifiableDataFeeder> feeder_temp = scfQueryInterface<iModifiableDataFeeder> (factory->GetFeeder ());
-       
+    //Query the Context Camera   
     csRef<iContextCamera> contextCamera = scfQueryInterface<iContextCamera> (editor->GetContext ());
-
+    //Query the engine
     csRef<iEngine> engine = csQueryRegistry<iEngine> (object_reg);
 
-    
+    //Query the current camera and view
     iCamera* camera = contextCamera->GetCamera();
     iView* view = contextCamera->GetView();
 
+    //Define the Start Beams and End Beams to calculate the intersection of the ray.
     csVector2 v2d (x, y);
     csVector3 v3d = view->InvProject (v2d, 1000.0f);    
     csVector3 startBeam = camera->GetTransform ().GetOrigin ();
     csVector3 endBeam = camera->GetTransform ().This2Other (v3d);  
     csVector3 position; 
   
+  //Query the mesh Object
     csRef<iMeshObject> meshObject = scfQueryInterface<iMeshObject> (terrain);
     
 
@@ -539,7 +554,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 
       
       RemoveModifier();
-      // Create the terrain modifier
+      // Create the terrain modifier if modifier mode is on.
      
       if (mode == 2)
       {    
@@ -555,8 +570,8 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 
      if (decalManager)
     {
-    // Create the decal material       
-    // Setup the decal template
+          
+    // Setup the decal template according to the activeMat.
     decalTemplate = decalManager->CreateDecalTemplate (activeMat);
     decalTemplate->SetDecalOffset (0.1f);
     decalTemplate->SetMaximumVertexCount (128000);
@@ -572,9 +587,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
     iMeshWrapper* meshWrapper = meshObject->GetMeshWrapper (); 
     
     decal = decalManager->CreateDecal (decalTemplate, meshWrapper, position, up, direction, rectSize, rectSize);  
-    
-
-    
+       
   } 
 
 
@@ -602,12 +615,11 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
 
    if (mode == 1)
    {
-    decal = nullptr;    
+    decal = nullptr; // Making the decal as nullptr Paints it onto the Terrain.    
    }
 
   }
 
-   
   CSTerrainEditSpace::EventListener::EventListener (CSTerrainEditSpace* editor)
     : scfImplementationType (this), editor (editor)
   {
@@ -635,7 +647,7 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
       int mouse_x = csMouseEventHelper::GetX (&event);
       int mouse_y = csMouseEventHelper::GetY (&event);
 
-      //printf ("mouse move at position %i-%i\n", mouse_x, mouse_y);
+      
       if(mouseready)
       {
       editor->UpdateModifier(mouse_x, mouse_y);
@@ -644,15 +656,11 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
     
     else if (event.Name == csevMouseClick(nameRegistry, 0))
     {
-      int mouse_x = csMouseEventHelper::GetX (&event);
-      int mouse_y = csMouseEventHelper::GetY (&event);
-
-      //printf ("mouse click at position %i-%i\n", mouse_x, mouse_y);
-
+      
       if (csMouseEventHelper::GetButton (&event) == csmbLeft)
       {
         editor->Paint();
-        //printf("Left Click");
+        
       }
 
       return false;
@@ -705,10 +713,8 @@ CS_PLUGIN_NAMESPACE_BEGIN (CSEditor)
     mode = 2;
   }
 
-  void CSTerrainEditSpace::ParseCell()
-  {
-   
-  }
+  
+///iModifiableListener Implementation
 
   void ModifiableListener::ValueChanged (CS::Utility::iModifiable* modifiable, size_t parameterIndex)
   {
